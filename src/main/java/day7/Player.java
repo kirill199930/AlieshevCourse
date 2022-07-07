f3af76f9fb86ac0db33d0b7d18659f9476e64b05
user <kirillxannanov1999@gmail.com>
2022-07-07 21:40:33 +0500

    CourseAlies

--- src/main/java/day7/Player.java
+++ src/main/java/day7/Player.java
@@ -1,4 +1,54 @@
 package day7;
 
 public class Player {
+    private int stamina;
+    private final int MAX_STAMINA = 100;
+    private final int MIN_STAMINA = 0;
+    private static int countPlayers;
+
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+        if (stamina == 0) {
+            countPlayers--;
+        }
+
+        if (stamina == 1) {
+            countPlayers++;
+        }
+
+        if (countPlayers >= 6) {
+            countPlayers--;
+        }
+        countPlayers++;
+
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+
+        if (stamina >= 1) {
+            System.out.println(stamina -= 1);
+        } else if (stamina == 0) {
+            System.out.println(stamina);
+        }
+
+        countPlayers--;
+    }
+
+    public static void info() {
+        if (countPlayers < 6) {
+            System.out.println("Команды неполные. На поле еще есть свободных мест");
+        } else {
+            System.out.println("На поле нет свободных мест");
+        }
+        //System.out.println(countPlayers);
+    }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -2,6 +2,28 @@ package day7;
 
 public class Task2 {
     public static void main(String[] args) {
+        Player player1 = new Player(90);
+        System.out.println("Количество игроков на поле после создания первого экземпляра " + Player.getCountPlayers());
+        Player player2 = new Player(100);
+        Player player3 = new Player(100);
+        Player player4 = new Player(90);
+        Player player5 = new Player(90);
+        Player player6 = new Player(90);
+        Player player7 = new Player(0);
+        Player player8 = new Player(1);
+
+        System.out.println("Количество игроков на поле после создания седьмого экземпляра " + Player.getCountPlayers());
+        Player.info();
+
+
+
+        System.out.println("Выносливость игрока 1 " + player1.getStamina());
+        player1.run();
+        Player.info();
+
+        player8.run();
+        player8.run();
+
 
     }
 }
